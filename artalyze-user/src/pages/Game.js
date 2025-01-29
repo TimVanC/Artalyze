89dab677918c0d69c17638f9076c89ff881e7804
Tim <Timmvanc@gmail.com>
2025-01-29 12:39:35 -0500

    ANNOUNCMENT: Currently about 99% of the project is working as expected in this commit

--- artalyze-user/src/pages/Game.js
+++ artalyze-user/src/pages/Game.js
@@ -58,6 +58,7 @@ const Game = () => {
     mistakeDistribution: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
     lastPlayedDate: null,
   });
+  
 
   // Helper function to save triesRemaining to localStorage
   const saveTriesToLocalStorage = (tries) => {
